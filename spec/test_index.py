5b94b979daa5c51c5fee64128f47a46a0feff872
Heorhi Sakovich <heorhi.sakovich@softswiss.com>
2025-08-31 17:24:34 +0400

    test: switch to pytest

--- spec/test_index.py
+++ spec/test_index.py
@@ -1,19 +1,18 @@
-import unittest
 from os import environ
 
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+URL = f"http://localhost:{environ['PORT']}"
 
-class TestIndex(unittest.TestCase):
-    def setUp(self):
-        self.driver = webdriver.Chrome()
-        self.url = f"http://localhost:{environ['PORT']}"
 
-    def tearDown(self):
-        self.driver.quit()
+@pytest.fixture
+def driver():
+    return webdriver.Chrome()
 
-    def test_index(self):
-        self.driver.get(self.url)
-        img = self.driver.find_element(by=By.TAG_NAME, value="img")
-        self.assertIsNotNone(img)
+
+def test_can_click_exactly_4_times_before_submitting(driver):
+    driver.get(URL)
+    img = driver.find_element(by=By.TAG_NAME, value="img")
+    assert img is not None
