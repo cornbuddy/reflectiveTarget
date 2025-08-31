2e8c70793272a85ad41eba25fbad1fd73ed4d9c9
Heorhi Sakovich <heorhi.sakovich@softswiss.com>
2025-08-31 14:35:30 +0400

    basic spec

---
+++ spec/test/test_index.py
@@ -0,0 +1,23 @@
+import unittest
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class TestIndex(unittest.TestCase):
+    def test_index(self):
+        driver = webdriver.Chrome()
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+        title = driver.title
+        self.assertIsNotNone(title)
+
+        driver.implicitly_wait(0.5)
+        text_box = driver.find_element(by=By.NAME, value="my-text")
+        submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
+        text_box.send_keys("Selenium")
+        submit_button.click()
+        message = driver.find_element(by=By.ID, value="message")
+        text = message.text
+        self.assertIsNotNone(text)
+
+        driver.quit()
