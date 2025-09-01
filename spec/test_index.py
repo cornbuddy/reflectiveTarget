7d55d39f0c194cabb08b093ad76239efd9e3e963
Heorhi Sakovich <heorhi.sakovich@softswiss.com>
2025-09-01 13:44:27 +0400

    fix: make spec work

--- spec/test_index.py
+++ spec/test_index.py
@@ -18,25 +18,6 @@ def driver():
     _driver.quit()
 
 
-def test_should_submit_shots_only_once(driver):
-    target = driver.find_element(By.TAG_NAME, "img")
-    for _ in range(ALLOWED_SHOTS):
-        target.click()
-    submit = driver.find_element(By.ID, "send")
-    submit.click()
-    driver.refresh()
-    shots = driver.find_elements(By.CSS_SELECTOR, "div.shot")
-    assert len(shots) == ALLOWED_SHOTS
-
-    submit = driver.find_element(By.ID, "send")
-    target = driver.find_element(By.TAG_NAME, "img")
-    shots = driver.find_elements(By.CSS_SELECTOR, "div.shot")
-    for _ in range(randint(1, 10)):
-        target.click()
-    submit.click()
-    assert len(shots) == ALLOWED_SHOTS
-
-
 def test_should_handle_mutliple_shots(driver):
     submit = driver.find_element(By.ID, "send")
     assert not submit.is_displayed()
@@ -55,4 +36,23 @@ def test_should_ignore_excessive_shots(driver):
     for _ in range(ALLOWED_SHOTS + randint(1, 10)):
         target.click()
     shots = driver.find_elements(By.CSS_SELECTOR, "div.shot")
+    assert len(shots) == ALLOWED_SHOTS
+
+
+def test_should_submit_shots_only_once(driver):
+    target = driver.find_element(By.TAG_NAME, "img")
+    for _ in range(ALLOWED_SHOTS):
+        target.click()
+    submit = driver.find_element(By.ID, "send")
+    submit.click()
+    driver.refresh()
+    shots = driver.find_elements(By.CSS_SELECTOR, "div.shot")
+    assert len(shots) == ALLOWED_SHOTS
+
+    submit = driver.find_element(By.ID, "send")
+    target = driver.find_element(By.TAG_NAME, "img")
+    shots = driver.find_elements(By.CSS_SELECTOR, "div.shot")
+    for _ in range(randint(1, 10)):
+        target.click()
+    submit.click()
     assert len(shots) == ALLOWED_SHOTS
