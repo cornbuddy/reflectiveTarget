3cbedd58a0983bb27998aba5e9ca4f977b8ac6c3
Heorhi Sakovich <heorhi.sakovich@gmail.com>
2025-09-22 14:51:18 +0400

    test(server): basic tests of database initialization

--- server/model/index.js
+++ server/model/index.js
@@ -1,5 +1,15 @@
-function initDatabase() {
-    throw Error("not implemented");
+async function initDatabase(client, databaseName) {
+    const res = await client.query(
+        `SELECT FROM pg_database WHERE datname = '${databaseName}'`,
+    );
+
+    if (res.rowCount === 0) {
+        console.log(`${databaseName} database not found, creating it.`);
+        await client.query(`CREATE DATABASE "${databaseName}";`);
+        console.log(`created database ${databaseName}.`);
+    } else {
+        console.log(`${databaseName} database already exists.`);
+    }
 }
 
 module.exports = { initDatabase };
