0fab71ec66c1319df7930ab32f0a66adf41c12e8
Heorhi Sakovich <heorhi.sakovich@gmail.com>
2025-10-01 15:18:35 +0400

    refactor: construct router object outside entry point

--- server/index.js
+++ server/index.js
@@ -2,16 +2,12 @@
 
 const join = require("path").join;
 
-const {
-    index,
-    saveShots,
-    getShots,
-} = require("./routes");
-
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 const express = require("express");
 const app = express();
+
+const router = require("./routes");
 
 const path = join(__dirname, "..", "client");
 app.use(express.static(path));
@@ -19,9 +15,7 @@ app.use(express.json());
 app.use(morgan("tiny"));
 app.use(cookieParser());
 
-app.get("/", index);
-app.get("/shots", getShots);
-app.post("/shots", saveShots);
+app.use(router);
 
 const port = process.env.PORT || 8080;
 app.listen(port);
