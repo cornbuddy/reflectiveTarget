c2e3a3d3153abedf4bd2d8edbb73be32a135f367
Heorhi Sakovich <heorhi.sakovich@gmail.com>
2025-10-01 12:59:29 +0400

    test: switch to firefox for testing

--- spec/test_index.py
+++ spec/test_index.py
@@ -1,10 +1,12 @@
 from os import environ
 from random import randint
+from shutil import which
 
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import chromedriver_autoinstaller
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
 
 ALLOWED_SHOTS = 4
 URL = f"http://localhost:{environ['PORT']}"
@@ -12,20 +14,20 @@ URL = f"http://localhost:{environ['PORT']}"
 
 @pytest.fixture
 def driver():
-    chromedriver_autoinstaller.install()
-    chrome_options = webdriver.ChromeOptions()
-    options = [
-        "--ignore-certificate-errors",
-        # These flags BELOW are recommended for stability when running Chrome
-        # in headless or containerized environments (such as GitHub Actions).
+    options = Options()
+    opts = [
         "--disable-gpu",
         "--no-sandbox",
         "--disable-dev-shm-usage",
-        "--remote-debugging-port=9222",
     ]
-    for option in options:
-        chrome_options.add_argument(option)
-    _driver = webdriver.Chrome(options=chrome_options)
+    for opt in opts:
+        options.add_argument(opt)
+    path = which("firefox.geckodriver")
+    service = Service(executable_path=path)
+    _driver = webdriver.Firefox(options=options, service=service)
+    _driver.set_window_size(1920, 1080)
+    _driver.maximize_window()
+    _driver.implicitly_wait(10)
     _driver.get(URL)
     yield _driver
 
