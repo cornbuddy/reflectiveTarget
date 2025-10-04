70cc9e324a98d2be137480835abd2e50553a77de
Heorhi Sakovich <heorhi.sakovich@gmail.com>
2025-10-04 22:04:08 +0400

    test: fail tests

--- server/lib/index.test.js
+++ server/lib/index.test.js
@@ -9,9 +9,16 @@ const supertest = require("supertest");
 
 const { makeApp } = require("./index");
 
-describe("router", () => {
+describe("endpoints", () => {
+    const app = makeApp();
+
+    test("get /health should succeed", async () => {
+        const resp = await supertest(app).get("/health");
+        expect(resp.status).toEqual(200);
+        expect(resp.headers["content-type"]).toMatch(/json/);
+    });
+
     test("get /shots should succeed", async () => {
-        const app = makeApp();
         const resp = await supertest(app).get("/shots");
         expect(resp.status).toEqual(200);
         expect(resp.headers["content-type"]).toMatch(/json/);
