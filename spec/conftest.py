9bd3c49db60ecc5a6dae94a10a93e20fcb636e93
Heorhi Sakovich <heorhi.sakovich@gmail.com>
2025-10-07 18:14:43 +0400

    test: make screenshots for spec test cases

--- spec/conftest.py
+++ spec/conftest.py
@@ -1,6 +1,14 @@
 import pytest
 
 
-@pytest.hookimpl(wrapper=True)
-def pytest_runtest_makereport(item, call):
-    raise RuntimeError("not implemented")
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item):
+    driver = item.funcargs.get("driver", None)
+    outcome = yield
+    test_report = outcome.get_result()
+    if driver and test_report.when == "call":
+        screenshot = driver.get_screenshot_as_base64()
+        pytest_html = item.config.pluginmanager.getplugin("html")
+        extras = getattr(test_report, "extra", [])
+        extras.append(pytest_html.extras.image(screenshot))
+        test_report.extra = extras
