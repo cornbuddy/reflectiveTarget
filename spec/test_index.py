4a521f61731e5ef321e2b5871a8f9084b58f69a9
Heorhi Sakovich <heorhi.sakovich@gmail.com>
2025-10-10 18:06:44 +0400

    test: share driver fixture

--- spec/test_index.py
+++ spec/test_index.py
@@ -1,40 +1,16 @@
 from os import environ
 from random import randint
-from shutil import which
 
-import pytest
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.service import Service
 
 ALLOWED_SHOTS = 4
 URL = f"http://localhost:{environ['PORT']}"
 
 
-@pytest.fixture
-def driver():
-    options = Options()
-    opts = [
-        "--disable-gpu",
-        "--no-sandbox",
-        "--disable-dev-shm-usage",
-        "--headless",
-    ]
-    for opt in opts:
-        options.add_argument(opt)
-    path = which("firefox.geckodriver")
-    service = Service(executable_path=path)
-    _driver = webdriver.Firefox(options=options, service=service)
-    _driver.set_window_size(1920, 1080)
-    _driver.get(URL)
-    _driver.implicitly_wait(10)
-    yield _driver
-
-    _driver.quit()
-
-
 def test_should_submit_shots_only_once(driver):
+    driver.get(URL)
+    driver.implicitly_wait(10)
+
     target = driver.find_element(By.TAG_NAME, "img")
     for _ in range(ALLOWED_SHOTS):
         target.click()
