e5862be80676615c46f217944453f7e00a31e2ff
Heorhi Sakovich <heorhi.sakovich@gmail.com>
2025-10-10 18:06:44 +0400

    test: share driver fixture

--- spec/conftest.py
+++ spec/conftest.py
@@ -1,4 +1,40 @@
+from shutil import which
+
 import pytest
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
+
+
+@pytest.fixture
+def driver():
+    options = Options()
+    opts = [
+        "--disable-gpu",
+        "--no-sandbox",
+        "--disable-dev-shm-usage",
+        "--headless",
+    ]
+    for opt in opts:
+        options.add_argument(opt)
+    path = which("firefox.geckodriver")
+    service = Service(executable_path=path)
+    _driver = webdriver.Firefox(options=options, service=service)
+    _driver.set_window_size(1920, 1080)
+    yield _driver
+
+    _driver.quit()
+
+
+def open_url(url):
+
+    @pytest.fixture
+    def fixture(driver):
+        driver.get(url)
+        driver.implicitly_wait(10)
+        return driver
+
+    return fixture
 
 
 @pytest.hookimpl(hookwrapper=True)
