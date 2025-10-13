cd0a785c48fdedc22e46428ffdad3d10812d4c69
Heorhi Sakovich <heorhi.sakovich@gmail.com>
2025-10-13 09:04:42 +0400

    chore: in progress

--- spec/test/test_authz.py
+++ spec/test/test_authz.py
@@ -8,7 +8,7 @@ LOGIN_URL = f"{URL}/login"
 SIGNUP_URL = f"{URL}/signup"
 
 
-@pytest.mark.parametrize("url", [(SIGNUP_URL)])
+@pytest.mark.parametrize("url", [SIGNUP_URL])
 def test_should_signup(driver):
     driver.find_element(By.NAME, "username").send_keys(USERNAME)
     driver.find_element(By.NAME, "password").send_keys(PASSWORD)
@@ -17,7 +17,7 @@ def test_should_signup(driver):
 
 
 @pytest.mark.order(after="test_should_register")
-@pytest.mark.parametrize("url", [(LOGIN_URL)])
+@pytest.mark.parametrize("url", [LOGIN_URL])
 def test_should_login(driver):
     driver.find_element(By.NAME, "username").send_keys(USERNAME)
     driver.find_element(By.NAME, "password").send_keys(PASSWORD)
