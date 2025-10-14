84491c6b26137814f7ca111900365f36fe39f49c
Heorhi Sakovich <heorhi.sakovich@gmail.com>
2025-10-14 13:57:33 +0400

    chore: in progress, making server more htmx friendly

--- server/lib/index.js
+++ server/lib/index.js
@@ -6,6 +6,8 @@ const { healthRouter, shotsRouter } = require("../routers");
 
 function makeApp(client) {
     const app = express();
+    app.set("view engine", "ejs");
+    app.set("views", "../views");
     app.use(express.json());
     app.use(morgan("tiny"));
     app.use(cookieParser());
