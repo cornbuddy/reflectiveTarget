ced3f6eeb4dcdcc785ace09087bb5da6d15c1fb6
Heorhi Sakovich <heorhi.sakovich@gmail.com>
2025-10-16 07:16:29 +0400

    chore: render index page on server

--- server/lib/index.js
+++ server/lib/index.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 const express = require("express");
@@ -7,12 +9,15 @@ const { healthRouter, shotsRouter } = require("../routers");
 function makeApp(client) {
     const app = express();
     app.set("view engine", "ejs");
-    app.set("views", "../views");
+    app.set("views", path.join(__dirname, "..", "views"));
     app.use(express.json());
     app.use(morgan("tiny"));
     app.use(cookieParser());
-    app.use("/health", healthRouter(client));
-    app.use("/shots", shotsRouter(client));
+    app.use("/api/health", healthRouter(client));
+    app.use("/api/shots", shotsRouter(client));
+    app.get("/", (_, res) => {
+        res.render("index");
+    });
     return app;
 }
 
