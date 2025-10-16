93dc39892a34e3b2a2205625d7204a6bc93ac68b
Heorhi Sakovich <heorhi.sakovich@gmail.com>
2025-10-16 09:24:05 +0400

    refactor: reduce code duplication

--- server/lib/index.test.js
+++ server/lib/index.test.js
@@ -23,13 +23,15 @@ describe("http endpoints", () => {
         await container.stop();
     });
 
-    describe("/", () => {
-        test("get should render index page", async () => {
-            const resp = await supertest(app).get("/");
-            expect(resp.status).toEqual(200);
-            expect(resp.headers["content-type"]).toMatch(/html/);
-            expect(resp.text).toContain("Рефлексивная мишень");
-        });
+    test.each([
+        { url: "/", text: "Reflective target" },
+        { url: "/login", text: "Login" },
+        { url: "/signup", text: "Signup" },
+    ])("view $url", async ({ url, text }) => {
+        const resp = await supertest(app).get(url);
+        expect(resp.status).toEqual(200);
+        expect(resp.headers["content-type"]).toMatch(/html/);
+        expect(resp.text).toContain(text);
     });
 
     describe("/api/health", () => {
@@ -51,7 +53,6 @@ describe("http endpoints", () => {
 
     describe("/api/shots", () => {
         test("get should succeed", async () => {
-            const app = makeApp();
             const resp = await supertest(app).get("/api/shots");
             expect(resp.status).toEqual(200);
             expect(resp.headers["content-type"]).toMatch(/json/);
