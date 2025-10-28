a489faf57f1d101c1170797f597d2f91480e15c7
Heorhi Sakovich <heorhi.sakovich@gmail.com>
2025-10-28 15:12:06 +0300

    chore: basic db integration

--- server/private/utils/utils_test.go
+++ server/private/utils/utils_test.go
@@ -2,8 +2,11 @@ package utils
 
 import (
 	"context"
+	"database/sql"
+	"fmt"
 	"testing"
 
+	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/stretchr/testify/assert"
 	"github.com/testcontainers/testcontainers-go"
 
@@ -11,12 +14,35 @@ import (
 )
 
 func TestInitDatabase(t *testing.T) {
+	t.Parallel()
+
 	ctx := context.TODO()
-	db, err := testdb.Setup(ctx)
+	dbContainer, err := testdb.Setup(ctx)
 	assert.NoError(t, err)
 
 	t.Cleanup(func() {
-		err := testcontainers.TerminateContainer(db)
+		err := testcontainers.TerminateContainer(dbContainer)
 		assert.NoError(t, err)
 	})
+
+	connStr, err := dbContainer.ConnectionString(ctx, "sslmode=disable")
+	assert.NoError(t, err)
+
+	db, err := sql.Open("pgx", connStr)
+	assert.NoError(t, err)
+
+	t.Cleanup(func() {
+		err := db.Close()
+		assert.NoError(t, err)
+	})
+
+	err = InitDatabase(db)
+	assert.NoError(t, err)
+
+	tables := []string{"users", "targets", "questions", "shots"}
+	for _, table := range tables {
+		query := fmt.Sprintf("select * from %s", table)
+		_, err := db.Query(query)
+		assert.NoError(t, err)
+	}
 }
--- server/test/db/db.go
+++ server/test/db/db.go
@@ -2,14 +2,24 @@ package db
 
 import (
 	"context"
+	"time"
 
+	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/modules/postgres"
+	"github.com/testcontainers/testcontainers-go/wait"
 )
 
 const Image = "postgres:18-alpine"
 
 func Setup(ctx context.Context) (*postgres.PostgresContainer, error) {
-	db, err := postgres.Run(ctx, Image)
+	str := wait.ForLog("database system is ready to accept connections").
+		WithOccurrence(2).
+		WithStartupTimeout(5 * time.Second)
+	opts := []testcontainers.ContainerCustomizer{
+		testcontainers.WithWaitStrategy(str),
+	}
+
+	db, err := postgres.Run(ctx, Image, opts...)
 	if err != nil {
 		return nil, err
 	}
