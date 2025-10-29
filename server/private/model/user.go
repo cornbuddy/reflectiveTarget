8ac238b7b072b5cae4c83ffee471c85fe32c6648
Heorhi Sakovich <heorhi.sakovich@gmail.com>
2025-10-29 21:42:48 +0300

    chore: in progress

--- server/private/model/user.go
+++ server/private/model/user.go
@@ -1,7 +1,12 @@
 package model
 
 import (
+	"fmt"
 	"net/url"
+)
+
+var (
+	ErrUserFormMissingKeys = fmt.Errorf("user form missing keys")
 )
 
 type User struct {
@@ -9,5 +14,10 @@ type User struct {
 }
 
 func NewUser(form url.Values) (*User, error) {
+	missingKeys := !form.Has("username") || !form.Has("password")
+	if missingKeys {
+		return nil, ErrUserFormMissingKeys
+	}
+
 	return &User{Username: "username"}, nil
 }
