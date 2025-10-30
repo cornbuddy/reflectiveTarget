c8be19b3c97d9a507886c6405e7246023dc48057
Heorhi Sakovich <heorhi.sakovich@gmail.com>
2025-10-30 15:54:13 +0300

    chore: basic hashing

--- server/private/model/password_test.go
+++ server/private/model/password_test.go
@@ -6,13 +6,13 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func BenchmarkNewPassword(b *testing.B) {
-	b.Fail("not implemented")
-}
-
-func TestPasswordHashingShouldBeDetermenistic(t *testing.T) {
+func TestPasswordHashingShouldNotBeDetermenistic(t *testing.T) {
 	plaintext := "kek"
-	pwd1 := NewPassword(plaintext)
-	pwd2 := NewPassword(plaintext)
-	assert.Equal(t, pwd1.Hash, pwd2.Hash)
+	pwd1, err := NewPassword(plaintext)
+	assert.NoError(t, err)
+
+	pwd2, err := NewPassword(plaintext)
+	assert.NoError(t, err)
+
+	assert.NotEqual(t, pwd1.Hash, pwd2.Hash)
 }
