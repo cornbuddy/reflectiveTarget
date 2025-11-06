9b324ea08380dad7c197f550722be899af0dc02e
Heorhi Sakovich <heorhi.sakovich@gmail.com>
2025-11-06 21:31:27 +0300

    chore: in progress

--- server/main.go
+++ server/main.go
@@ -2,12 +2,43 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
+
+var ErrNoEnvVar = fmt.Errorf("no environment variable")
 
 func main() {
 	fmt.Println("hello world")
 }
 
 func Init() (*Config, error) {
+	var password, user, database, host string
+
+	if pwd, ok := os.LookupEnv("PGPASSWORD"); !ok {
+		return nil, fmt.Errorf("%w: %s", ErrNoEnvVar, "PGPASSWORD")
+	} else {
+		password = pwd
+	}
+
+	if usr, ok := os.LookupEnv("PGUSER"); !ok {
+		return nil, fmt.Errorf("%w: %s", ErrNoEnvVar, "PGUSER")
+	} else {
+		user = usr
+	}
+
+	if db, ok := os.LookupEnv("PGDATABASE"); !ok {
+		return nil, fmt.Errorf("%w: %s", ErrNoEnvVar, "PGDATABASE")
+	} else {
+		database = db
+	}
+
+	if h, ok := os.LookupEnv("PGHOST"); !ok {
+		return nil, fmt.Errorf("%w: %s", ErrNoEnvVar, "PGHOST")
+	} else {
+		host = h
+	}
+
+	fmt.Println(password, user, database, host)
+
 	return nil, fmt.Errorf("not implemented")
 }
