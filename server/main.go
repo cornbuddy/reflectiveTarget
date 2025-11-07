90cf6e5942891a147fe8bd7a8ced0c571d9d15fa
Heorhi Sakovich <heorhi.sakovich@gmail.com>
2025-11-07 17:27:02 +0300

    chore: basic routing

--- server/main.go
+++ server/main.go
@@ -3,17 +3,49 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 
-	"github.com/cornbuddy/reflectiveTarget/server/private/daos"
-	"github.com/cornbuddy/reflectiveTarget/server/private/utils"
 	_ "github.com/jackc/pgx/v5/stdlib"
+
+	"github.com/cornbuddy/reflectiveTarget/server/private/daos"
+	"github.com/cornbuddy/reflectiveTarget/server/private/handlers"
+	"github.com/cornbuddy/reflectiveTarget/server/private/utils"
 )
+
+type Config struct {
+	*sql.DB
+	daos.UserDao
+}
 
 var ErrNoEnvVar = fmt.Errorf("no environment variable")
 
 func main() {
-	fmt.Println("hello world")
+	config, err := Init()
+	if err != nil {
+		log.Fatalf("failed to init application: %s", err)
+	}
+
+	health := handlers.HealthRouter{
+		DB: config.DB,
+	}
+	authz := handlers.AuthzRouter{
+		UserDao: config.UserDao,
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /login", authz.GetLogin)
+	mux.HandleFunc("GET /signup", authz.GetSignup)
+	mux.HandleFunc("POST /login", authz.PostLogin)
+	mux.HandleFunc("POST /signup", authz.PostSignup)
+	mux.HandleFunc("GET /api/health", health.Get)
+
+	log.Println("starting http server")
+	// TODO: provide port as a config
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatalf("failed to start http server: %s", err)
+	}
 }
 
 func Init() (*Config, error) {
