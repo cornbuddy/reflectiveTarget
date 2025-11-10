23b306904c5f6253b02d5ad01be939baf10b05d5
Heorhi Sakovich <heorhi.sakovich@gmail.com>
2025-11-10 08:42:53 +0300

    fix: handle index properly

--- server/private/handlers/index.go
+++ server/private/handlers/index.go
@@ -5,5 +5,10 @@ import (
 )
 
 func (r IndexRouter) Get(resp http.ResponseWriter, req *http.Request) {
+	if req.URL.Path != "/" {
+		http.NotFound(resp, req)
+		return
+	}
+
 	engine.Render(resp, "views/index.tmpl", nil)
 }
