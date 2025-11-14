6be728132410cf147569dc6764910d9b2c06e494
Heorhi Sakovich <heorhi.sakovich@gmail.com>
2025-11-14 08:42:13 +0300

    test: rename variable

--- spec/test/test_authz.py
+++ spec/test/test_authz.py
@@ -5,17 +5,17 @@ from constants import PASSWORD, USERNAME, URL
 
 def test_should_signup(dsl):
     driver = dsl.signup(USERNAME, PASSWORD)
-    cookie = driver.get_cookie("session-token")
+    session = driver.get_cookie("session-token")
     assert driver.current_url == f"{URL}/"
-    assert cookie is not None
+    assert session is not None
 
 
 @pytest.mark.order(after="test_should_signup")
 def test_should_login(dsl):
-    cookie = dsl.driver.get_cookie("session-token")
-    assert cookie is None
+    session = dsl.driver.get_cookie("session-token")
+    assert session is None
 
     driver = dsl.login(USERNAME, PASSWORD)
-    cookie = driver.get_cookie("session-token")
-    assert cookie is not None
+    session = driver.get_cookie("session-token")
+    assert session is not None
     assert driver.current_url == f"{URL}/"
