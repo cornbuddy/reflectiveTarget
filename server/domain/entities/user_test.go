4a3603221680aeca6abb3981fcc1d9a62ee20936
Heorhi Sakovich <heorhi.sakovich@gmail.com>
2025-12-02 17:31:49 +0300

    chore: in progress

--- server/domain/entities/user_test.go
+++ server/domain/entities/user_test.go
@@ -4,31 +4,51 @@ import (
 	"net/url"
 	"testing"
 
+	"github.com/cornbuddy/reflectiveTarget/server/domain/valueobjects"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestNewUserShouldReturnErrorWhenFormMissingRequiredKeys(t *testing.T) {
+func TestUserFieldsShouldBePopulatedAccordingly(t *testing.T) {
 	t.Parallel()
 
-	fields := []string{"username", "password"}
-	for _, field := range fields {
-		form := url.Values{}
-		form.Set(field, "kek")
-		user, err := NewUser(form)
-		assert.ErrorIs(t, err, ErrUserFormMissingKeys)
-		assert.Nil(t, user)
+	type testCase struct {
+		form url.Values
+		user User
 	}
-}
 
-func TestNewUserShouldHashPassword(t *testing.T) {
-	t.Parallel()
+	testCases := []testCase{{
+		form: url.Values{},
+		user: User{},
+	}, {
+		form: url.Values{"username": []string{"kek"}},
+		user: User{Username: "kek"},
+	}, {
+		form: url.Values{"password": []string{"kek"}},
+		user: User{
+			Password: valueobjects.Password{
+				Hash:      "not kek",
+				Plaintext: "kek",
+			},
+		},
+	}, {
+		form: url.Values{
+			"password": []string{"kek"},
+			"username": []string{"kek"},
+		},
+		user: User{
+			Username: "kek",
+			Password: valueobjects.Password{
+				Hash:      "not kek",
+				Plaintext: "kek",
+			},
+		},
+	}}
 
-	form := url.Values{}
-	form.Set("username", "username")
-	form.Set("password", "password")
-	user, err := NewUser(form)
-	assert.NoError(t, err)
-	assert.Equal(t, form.Get("username"), user.Username)
-	assert.NotEqual(t, form.Get("password"), user.Password.Hash)
-	assert.NotEmpty(t, user.Password)
+	for _, tc := range testCases {
+		user, err := NewUser(tc.form)
+		require.NoError(t, err)
+		assert.Equal(t, tc.user, user)
+	}
+
 }
