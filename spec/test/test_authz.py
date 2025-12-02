67017eca94cb813815ee5574c7cbc3de9a6aeaa7
Heorhi Sakovich <heorhi.sakovich@gmail.com>
2025-12-02 17:08:45 +0300

    test: align code with recent refactoring

--- spec/test/test_authz.py
+++ spec/test/test_authz.py
@@ -2,13 +2,13 @@ from constants import PASSWORD, USERNAME, URL
 
 
 def test_user_should_be_able_to_logout(authorized_user):
-    driver = authorized_user.logout()
-    assert driver.current_url == f"{URL}/"
+    authorized_user.logout()
+    assert authorized_user.driver.current_url == f"{URL}/"
 
 
 def test_user_should_be_able_to_login(authorized_user):
-    driver = authorized_user.login(USERNAME, PASSWORD)
-    assert driver.current_url == f"{URL}/"
+    authorized_user.login(USERNAME, PASSWORD)
+    assert authorized_user.driver.current_url == f"{URL}/"
 
 
 def test_password_should_be_validated_upon_signup(anonymous_user):
