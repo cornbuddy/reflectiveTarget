48e539985bac045b5a39adcf8ec5cfef2ae0b247
Heorhi Sakovich <heorhi.sakovich@gmail.com>
2026-02-27 15:23:24 +0300

    refactor: move tests initializations to main test file

--- server/infra/repositories/main_test.go
+++ server/infra/repositories/main_test.go
@@ -7,6 +7,11 @@ import (
 	"os"
 	"testing"
 
+	"github.com/bloomberg/go-testgroup"
+
+	"github.com/cornbuddy/reflectiveTarget/server/domain/aggregations"
+	"github.com/cornbuddy/reflectiveTarget/server/domain/entities"
+	vo "github.com/cornbuddy/reflectiveTarget/server/domain/valueobjects"
 	"github.com/cornbuddy/reflectiveTarget/server/infra/repositories"
 	"github.com/cornbuddy/reflectiveTarget/server/infra/utils"
 	testutils "github.com/cornbuddy/reflectiveTarget/server/test/utils"
@@ -18,6 +23,42 @@ var (
 	db         *sql.DB
 	targetRepo repositories.TargetRepo
 )
+
+type TargetGetTests struct {
+	*aggregations.Target
+}
+
+func (suite *TargetGetTests) PreGroup(t *testgroup.T) {
+	owner, err := entities.NewUser("username", "password")
+	t.Require.NoError(err)
+	t.Require.NoError(testutils.InsertUser(db, owner))
+
+	suite.Target = &aggregations.Target{
+		Name:  "test",
+		Owner: *owner,
+	}
+	t.Require.NoError(testutils.InsertTarget(db, suite.Target))
+
+	targetId := suite.Target.ID
+	questions := vo.Questions{
+		{Text: "kek1?"},
+		{Text: "kek2?"},
+	}
+	t.Require.NoError(testutils.InsertQuestions(db, &questions, targetId))
+
+	shots := vo.Shots{
+		{X: 1, Y: 100},
+		{X: 100, Y: 1},
+	}
+	t.Require.NoError(testutils.InsertShots(db, &shots, targetId, "kek"))
+
+	suite.Target.Questions = questions
+	suite.Target.Shots = shots
+}
+
+type TargetSaveTests struct{}
+
+type TargetListTargetNames struct{}
 
 func TestMain(m *testing.M) {
 	cleanUpDb, testDb, err := testutils.SetupTestDb(ctx)
--- server/infra/repositories/target_test.go
+++ server/infra/repositories/target_test.go
@@ -4,44 +4,7 @@ import (
 	"testing"
 
 	"github.com/bloomberg/go-testgroup"
-
-	"github.com/cornbuddy/reflectiveTarget/server/domain/aggregations"
-	"github.com/cornbuddy/reflectiveTarget/server/domain/entities"
-	vo "github.com/cornbuddy/reflectiveTarget/server/domain/valueobjects"
-	testutils "github.com/cornbuddy/reflectiveTarget/server/test/utils"
 )
-
-type TargetGetTests struct {
-	*aggregations.Target
-}
-
-func (suite *TargetGetTests) PreGroup(t *testgroup.T) {
-	owner, err := entities.NewUser("username", "password")
-	t.Require.NoError(err)
-	t.Require.NoError(testutils.InsertUser(db, owner))
-
-	suite.Target = &aggregations.Target{
-		Name:  "test",
-		Owner: *owner,
-	}
-	t.Require.NoError(testutils.InsertTarget(db, suite.Target))
-
-	targetId := suite.Target.ID
-	questions := vo.Questions{
-		{Text: "kek1?"},
-		{Text: "kek2?"},
-	}
-	t.Require.NoError(testutils.InsertQuestions(db, &questions, targetId))
-
-	shots := vo.Shots{
-		{X: 1, Y: 100},
-		{X: 100, Y: 1},
-	}
-	t.Require.NoError(testutils.InsertShots(db, &shots, targetId, "kek"))
-
-	suite.Target.Questions = questions
-	suite.Target.Shots = shots
-}
 
 func (*TargetGetTests) ShouldReturnNilIfTargetDoesNotExist(t *testgroup.T) {
 	got, err := targetRepo.Get(ctx, 69)
@@ -56,13 +19,9 @@ func (suite *TargetGetTests) ShouldReturnTargetIfExist(t *testgroup.T) {
 	t.EqualValues(*suite.Target, *got)
 }
 
-type TargetSaveTests struct{}
-
 func (*TargetSaveTests) ShouldSaveTarget(t *testgroup.T) {
 	t.Require.NoError(targetRepo.Save(ctx, nil))
 }
-
-type TargetListTargetNames struct{}
 
 func (*TargetListTargetNames) ShouldListTargetNames(t *testgroup.T) {
 	targets, err := targetRepo.ListTargetNamesOfUser(ctx, "kek")
