5a755c43a8c9b2dbaccd3dad4c44b6990a38c2f6
Heorhi Sakovich <heorhi.sakovich@gmail.com>
2026-05-26 00:38:43 +0300

    chore: make test more readable

--- spec/test/test_target.py
+++ spec/test/test_target.py
@@ -51,8 +51,9 @@ def test_user_should_be_able_to_edit_its_target(
     want_question = "new question value"
     update_page.update_target(TARGET_NAME, [want_question])
     page.get_by_text(TARGET_NAME).click()
-    got_question = page.locator(LOCATORS["question"](0)).get_attribute("value")
-    assert got_question == want_question
+    expect(
+        page.locator(LOCATORS["question"](0)),
+    ).to_have_attribute("value", want_question)
 
 
 @pytest.mark.skip
